1dd2dc67941a9eb8b7348564905054f9e19f00ab
Ville Skyttä <ville.skytta@iki.fi>
2015-10-20 10:38:57 +0300

    Embed pointer image in data URL

--- server/views/remote-mouse.js
+++ server/views/remote-mouse.js
@@ -2,10 +2,12 @@
   'use strict';
 
   var pointer = document.createElement('img');
-  pointer.setAttribute('src', 'http://localhost:9000/pointer.png'); // TODO don't hardcode URL
+  // Pointer tinkered from:
+  // https://openclipart.org/detail/222076/white-mouse-cursor-arrow
+  pointer.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAUCAMAAACzvE1FAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3woUByEHrcNK9gAAAH5QTFRF9vb2%2F%2F%2F%2F6urqAQEBEhISFhYWFxcXLS0tMjIyNDQ0ODg4Pj4%2BRUVFSUlJS0tLT09PWFhYXl5eX19fcnJydXV1jo6Oj4%2BPkpKSmJiYmZmZm5ubn5%2BfoqKiqampsrKywsLCw8PDxcXFzs7O0tLS2tra3Nzc5%2Bfn9%2Ff3%2Bfn5%2F%2F%2F%2FDUkwHwAAAAN0Uk5TAADMaKRKRQAAAAFiS0dEKcq3hSQAAAB0SURBVBjTZdBHEoMwEETRaRxIDmRrnMAkw%2F0vyAIxCPGXb9NVTbAiAC8bDsoC7662EAyGEIBgNGQGQzSssoCIwCIraBH4MD%2BOSuD3TxNmfpKG6vTubsZK5TfheG0Fzl6PqP5mAq4DlHHuCjgAcClIYPfYpgmojQvVxGP2NgAAAABJRU5ErkJggg%3D%3D');
   pointer.style.position = 'absolute';
   pointer.style.height = '20px';
-  pointer.style.width = '20px';
+  pointer.style.width = '16px';
   pointer.style['z-index'] = 100000;
 
   var top = 0;
